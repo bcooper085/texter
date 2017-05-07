d9162be9af47a094f631011d5e386fd21fb3a623
brandon <bcooper085@gmail.com>
2017-05-07 12:09:33 -0700

    app texter app with Twilio API

---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "ACb0fb471fb3ee4796d01498ab1dbf0bd8";
+        public static string AuthToken = "27f2921bad5ac4e9176d64864d0e0ce7";
+    }
+}
